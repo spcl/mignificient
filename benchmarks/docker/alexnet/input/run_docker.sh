90b4734427fc5086472bf70f9681153845e92b30
EricPyZhou <ericzpy1998@outlook.com>
2024-03-07 22:20:58 +0100

    alexnet dragt

---
+++ benchmarks/docker/alexnet/input/run_docker.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 \
+sarus run --mount=type=bind,source=/users/pzhou/projects/mignificient/benchmarks/docker/alexnet/input,destination=/bench zhoupengyu1998/alexnet-test:v1.0 python3 /app/run.py /bench/dog.jpg
---
+++ benchmarks/docker/alexnet/run.py
@@ -0,0 +1,25 @@
+import sys
+import base64
+import json
+import kernel
+
+def main(image_path):
+    with open(image_path, "rb") as image_file:
+        image_bytes = image_file.read()
+    
+    # turn the image bytes into Base64 string
+    image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+    
+    json_obj = {
+        "image_data": image_base64,
+    }
+    
+    payload = json.dumps(json_obj)
+    
+    ret = kernel.alexnet(payload)
+    print("get ret category id", ret)
+
+
+if __name__ == "__main__":
+    image_path = sys.argv[1]
+    main(image_path)
