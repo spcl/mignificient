c725812dfd9d8729976c671907ba56016de42a66
Marcin Copik <mcopik@gmail.com>
2024-10-01 04:55:45 +0200

    [invoker] Add the option to invoke functions other than batches

--- invoker/cli.cpp
+++ invoker/cli.cpp
@@ -18,49 +18,108 @@ struct InvocatonResult
   int invocation;
 };
 
-int main(int argc, char ** argv)
+void independent(const std::string& address, int iterations, int parallel_requests, Json::Value& input_data, bool different_users, std::vector<InvocatonResult>& results)
 {
-  Json::Value config;
+  std::vector<drogon::HttpRequestPtr> requests;
+  std::vector<drogon::HttpClientPtr> clients;
 
-  if(argc == 2) {
+  //trantor::Logger::setLogLevel(trantor::Logger::kTrace);
 
-    spdlog::info("Reading configuration from {}", argv[1]);
-    std::ifstream cfg_data{argv[1]};
+  for (int j = 0; j < parallel_requests; ++j) {
 
-    if(!cfg_data.is_open()) {
-      spdlog::error("Failed to open file with JSON config!");
-      return 1;
+    for(int i = 0; i < iterations; ++i) {
+
+      drogon::HttpRequestPtr req;
+      if(input_data.isArray()) {
+
+        input_data[j]["uuid"] = fmt::format("invoc-{}-{}", i, j);
+        if(different_users) {
+          input_data[j]["user"] = fmt::format("user-{}", j);
+        } else {
+          input_data[j]["user"] = "user";
+        }
+        req = drogon::HttpRequest::newHttpJsonRequest(input_data[j]);
+
+        std::cerr << input_data[j] << std::endl;
+      } else {
+
+        input_data["uuid"] = fmt::format("invoc-{}-{}", i, j);
+        if(different_users) {
+          input_data["user"] = fmt::format("user-{}", j);
+        } else {
+          input_data["user"] = "user";
+        }
+        req = drogon::HttpRequest::newHttpJsonRequest(input_data);
+
+        std::cerr << input_data << std::endl;
+      }
+
+      req->setMethod(drogon::Post);
+      req->setPath("/invoke");
+
+      requests.push_back(std::move(req));
+      clients.push_back(drogon::HttpClient::newHttpClient(address));
+
     }
 
-    Json::CharReaderBuilder builder;
-    JSONCPP_STRING errs;
-    if (!Json::parseFromStream(builder, cfg_data, &config, &errs)) {
-      spdlog::error("Failed to parse JSON config! {}", errs);
-      return 1;
-    }
-
-  } else {
-
-    spdlog::info("Reading configuration from stdin");
-
-    Json::CharReaderBuilder builder;
-    JSONCPP_STRING errs;
-    if (!Json::parseFromStream(builder, std::cin, &config, &errs)) {
-      spdlog::error("Failed to parse JSON config! {}", errs);
-      return 1;
-    }
   }
 
-  std::string address = config["address"].asString();
-  int iterations = config["iterations"].asInt();
-  int parallel_requests = config["parallel-requests"].asInt();
-  bool different_users = config["different-users"].asBool();
-  Json::Value input_data = config["inputs"];
+  auto begin = std::chrono::high_resolution_clock::now();
+  std::vector<std::thread> threads;
+  for (int i = 0; i < parallel_requests; ++i) {
 
+    threads.emplace_back(
+      [iterations, &results, &requests, &clients, parallel_requests, i]() {
+
+        for(int j = 0; j < iterations; ++j) {
+
+          auto& res = results[i*iterations + j];
+          spdlog::error("Send worker {}, iter {}", i, j);
+
+          res.start = std::chrono::high_resolution_clock::now();
+          auto [result, response] = clients[i]->sendRequest(requests[i*iterations + j]);
+          res.end = std::chrono::high_resolution_clock::now();
+
+          if(result != drogon::ReqResult::Ok || response->getStatusCode() != drogon::HttpStatusCode::k200OK) {
+            spdlog::error("Failed invocation! Result {} Status {} Body {}", drogon::to_string_view(result), response->getStatusCode(), response->body());
+          } else {
+            spdlog::info("Finished invocation. Result {} Body {}", drogon::to_string_view(result), response->body());
+          }
+
+        }
+
+      }
+    );
+
+  }
+
+  for(auto & t : threads) {
+    t.join();
+  }
+  auto end = std::chrono::high_resolution_clock::now();
+
+  spdlog::info("Total time: {}", std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1000.0);
+
+  for(int i = 0; i < parallel_requests; ++i) {
+
+    for (int j = 0; j < iterations; ++j) {
+
+      auto& res = results[i*parallel_requests + j];
+      spdlog::info(
+        "Client {}, Invocation {}, Time {}",
+        i, j,
+        std::chrono::duration_cast<std::chrono::microseconds>(res.end-res.start).count()
+      );
+
+    }
+
+  }
+}
+
+void batches(const std::string& address, int iterations, int parallel_requests, Json::Value& input_data, bool different_users, std::vector<InvocatonResult>& results)
+{
   std::vector<drogon::HttpRequestPtr> requests;
-  std::vector<InvocatonResult> results;
   std::vector<drogon::HttpClientPtr> clients;
-  results.resize(iterations * parallel_requests);
 
   //trantor::Logger::setLogLevel(trantor::Logger::kTrace);
   for(int i = 0; i < iterations; ++i) {
@@ -99,12 +158,6 @@ int main(int argc, char ** argv)
     }
 
   }
-
-  drogon::app().setThreadNum(parallel_requests);
-
-  std::thread drogon_thread([]() {
-      drogon::app().run();
-  });
 
   // Would be simpler with std::atoic::wait but requires C++20
   std::mutex mutex;
@@ -156,6 +209,62 @@ int main(int argc, char ** argv)
 
     }
 
+  }
+}
+
+int main(int argc, char ** argv)
+{
+  Json::Value config;
+
+  if(argc == 2) {
+
+    spdlog::info("Reading configuration from {}", argv[1]);
+    std::ifstream cfg_data{argv[1]};
+
+    if(!cfg_data.is_open()) {
+      spdlog::error("Failed to open file with JSON config!");
+      return 1;
+    }
+
+    Json::CharReaderBuilder builder;
+    JSONCPP_STRING errs;
+    if (!Json::parseFromStream(builder, cfg_data, &config, &errs)) {
+      spdlog::error("Failed to parse JSON config! {}", errs);
+      return 1;
+    }
+
+  } else {
+
+    spdlog::info("Reading configuration from stdin");
+
+    Json::CharReaderBuilder builder;
+    JSONCPP_STRING errs;
+    if (!Json::parseFromStream(builder, std::cin, &config, &errs)) {
+      spdlog::error("Failed to parse JSON config! {}", errs);
+      return 1;
+    }
+  }
+
+  std::string mode = config["mode"].asString();
+  std::string address = config["address"].asString();
+  int iterations = config["iterations"].asInt();
+  int parallel_requests = config["parallel-requests"].asInt();
+  bool different_users = config["different-users"].asBool();
+  Json::Value input_data = config["inputs"];
+
+  drogon::app().setThreadNum(parallel_requests);
+
+  std::thread drogon_thread([]() {
+      drogon::app().run();
+  });
+
+  std::vector<InvocatonResult> results;
+  results.resize(iterations * parallel_requests);
+
+  if(mode == "batches") {
+    batches(address, iterations, parallel_requests, input_data, different_users, results);
+  } else if(mode == "independent") {
+    independent(address, iterations, parallel_requests, input_data, different_users, results);
   }
 
   drogon::app().quit();
