35e1d9fae03f5b313053fad5c0827d33e1215206
Marcin Copik <mcopik@gmail.com>
2026-02-05 11:39:56 +0100

    [executor] Add iceoryx2 backend

--- executor/include/mignificient/executor/executor.hpp
+++ executor/include/mignificient/executor/executor.hpp
@@ -2,6 +2,7 @@
 #define __MIGNIFICIENT_EXECUTOR_EXECUTOR_HPP__
 
 #include <cstdint>
+#include <iox2/service_type.hpp>
 #include <string>
 #include <optional>
 
@@ -12,6 +13,8 @@
 #include <iceoryx_hoofs/posix_wrapper/signal_handler.hpp>
 
 #include "function.hpp"
+
+#include <mignificient/ipc/config.hpp>
 
 #ifdef MIGNIFICIENT_WITH_ICEORYX2
 #include <iox2/iceoryx2.hpp>
@@ -49,9 +52,65 @@ namespace mignificient { namespace executor {
     size_t size;
   };
 
+  struct CommunicationIceoryxV1
+  {
+
+    std::optional<iox::popo::Publisher<InvocationResult>> client;
+    std::optional<iox::popo::UntypedSubscriber> orchestrator;
+    std::optional<iox::popo::WaitSet<>> waitset;
+
+    static iox::popo::WaitSet<>* _waitset;
+
+    std::optional<iox::posix::SignalGuard> sigint;
+    std::optional<iox::posix::SignalGuard> sigterm;
+
+    std::optional<iox::popo::Sample<InvocationResult, iox::mepoo::NoUserHeader>> _result;
+    std::optional<iox::popo::Sample<InvocationResult, iox::mepoo::NoUserHeader>> _yield_msg;
+
+    CommunicationIceoryxV1(const std::string& name);
+
+    void gpu_yield();
+    void register_runtime();
+    void finish(int size);
+
+    InvocationData loop_wait();
+
+    const void* last_message;
+  };
+
+#ifdef MIGNIFICIENT_WITH_ICEORYX2
+  struct CommunicationIceoryxV2
+  {
+    std::optional<iox2::Node<iox2::ServiceType::Ipc>> iox2_node;
+
+    std::optional<iox2::PortFactoryEvent<iox2::ServiceType::Ipc>> client_event_listen;
+    std::optional<iox2::PortFactoryEvent<iox2::ServiceType::Ipc>> client_event_notify;
+    std::optional<iox2::WaitSet<iox2::ServiceType::Ipc>> waitset;
+
+    std::optional<iox2::Publisher<iox2::ServiceType::Ipc, mignificient::executor::InvocationResult, void>> client_send;
+    std::optional<iox2::Notifier<iox2::ServiceType::Ipc>> client_notifier;
+
+    std::optional<iox2::Subscriber<iox2::ServiceType::Ipc, mignificient::executor::Invocation, void>> orchestrator_recv;
+    std::optional<iox2::Listener<iox2::ServiceType::Ipc>> orchestrator_listener;
+    std::optional<iox2::WaitSetGuard<iox2::ServiceType::Ipc>> orchestrator_listener_guard;
+
+    std::optional<iox2::Sample<iox2::ServiceType::Ipc, Invocation, void>> _last_message;
+    std::optional<iox2::SampleMutUninit<iox2::ServiceType::Ipc, InvocationResult, void>> _result;
+    std::optional<iox2::SampleMutUninit<iox2::ServiceType::Ipc, InvocationResult, void>> _yield_msg;
+
+    CommunicationIceoryxV2(const std::string& id);
+
+    void gpu_yield();
+    void register_runtime();
+    void finish(int size);
+
+    InvocationData loop_wait();
+  };
+#endif
+
   struct Runtime {
 
-    Runtime(const std::string& name);
+    Runtime(mignificient::ipc::IPCBackend config, const std::string& name);
 
     InvocationData loop_wait();
 
@@ -63,23 +122,19 @@ namespace mignificient { namespace executor {
 
     InvocationResultData result();
 
+    static void sigHandler(int sig);
+
+    static bool quit;
+
   private:
 
-    std::optional<iox::popo::Publisher<InvocationResult>> client;
-    std::optional<iox::popo::UntypedSubscriber> orchestrator;
-    std::optional<iox::popo::WaitSet<>> waitset;
+    std::optional<CommunicationIceoryxV1> _comm_v1;
+#ifdef MIGNIFICIENT_WITH_ICEORYX2
+    std::optional<CommunicationIceoryxV2> _comm_v2;
+#endif
 
-    std::optional<iox::posix::SignalGuard> sigint;
-    std::optional<iox::posix::SignalGuard> sigterm;
+    mignificient::ipc::IPCBackend _backend;
 
-    std::optional<iox::popo::Sample<InvocationResult, iox::mepoo::NoUserHeader>> _result;
-    std::optional<iox::popo::Sample<InvocationResult, iox::mepoo::NoUserHeader>> _yield_msg;
-
-    const void* last_message;
-
-    static bool _quit;
-    static iox::popo::WaitSet<>* _waitset;
-    static void _sigHandler(int sig);
   };
 
 }}
